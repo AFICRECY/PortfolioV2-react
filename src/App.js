23ecf12152d27120b5093dacebc7c19f30708fff
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-26 22:51:31 -0700

    input basic file sructure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
-import './App.css';
-import BucketList from './components/BucketList';
+// import React, { useState } from 'react';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Contact from './pages/Contact';
+// import Resume from './pages/Resume';
+// import Footer from './pages/Footer';
 
-function App() {
-  return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
-  );
-}
+// function App() {
+//   const [currentPage, setCurrentPage] = useState('Home');
 
-export default App;
+
+//   const renderPage = () => {
+//     if (currentPage === 'Home') {
+//       return <Home />;
+//     }
+//     if (currentPage === 'About') {
+//       return <About />;
+//     }
+//     if (currentPage === 'Portfolio') {
+//       return <Portfolio />;
+//     }
+//     if (currentPage === 'Resume') {
+//       return <Resume />;
+//       return <Contact />;
+//   };
+
+//   }
+
+//   const handlePageChange = (page) => setCurrentPage(page);
+
+//   return (
+//     <div>
+//       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      
+//       {renderPage()}
+//       <Footer/>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import './App.css';
+// // // import BucketList from './components/BucketList';
+
+// // // function App() {
+// // //   return (
+// // //     <div className="bucket-app">
+// // //       {/* <BucketList /> */}
+// // //     </div>
+// // //   );
+// // // }
+
+// // export default App;
