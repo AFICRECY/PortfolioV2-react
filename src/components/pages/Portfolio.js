bc7443b1c674ea1a235b501c83b7efd48ee2cd68
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-30 22:26:06 -0700

    added the links to all the repositories in the card text

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -38,6 +38,9 @@ function Portfolio() {
           <MDBCardBody className="CardBody">
            <MDBCardTitle className='pt-4'>Cinexplorer Movie Search</MDBCardTitle>
             <MDBCardText>
+            <a href="https://github.com/CodeNameNoah/Movie-Indexer" target="_blank"
+            rel="noreferrer">(Link to repository)</a>  
+            <br/>
             I am excited to tell you about the application my team and I made for movie enthusiasts. Our application is designed to provide users with an easy and efficient way to search for information about their favorite movies. This application utilizes OMDb's Movie API.
             </MDBCardText>
           </MDBCardBody>
@@ -58,7 +61,9 @@ function Portfolio() {
           <MDBCardBody className="CardBody">
             <MDBCardTitle className='pt-4'>Feather Pad Text Editor</MDBCardTitle>
             <MDBCardText>
-              <a href =""></a>
+            <a href="https://github.com/AFICRECY/Feather-Pad-texteditor" target="_blank"
+            rel="noreferrer">(Link to repository)</a>  
+            <br/>
             This installable digital notebook appplication serves as a modern counterpart to traditional paper notebooks, enabling users to conveniently organize their notes and information and auto save on the go.
             </MDBCardText>
           </MDBCardBody>
@@ -79,6 +84,9 @@ function Portfolio() {
           <MDBCardBody className="CardBody">
             <MDBCardTitle className='pt-4'>Hops Haven</MDBCardTitle>
             <MDBCardText>
+            <a href="https://github.com/CodeNameNoah/Hops-Haven" target="_blank"
+            rel="noreferrer">(Link to repository)</a>  
+            <br/>
               My Team and I created a web application allows access to a beer API that provides users with a comprehensive resource for exploring the world of beer and discovering new and exciting options. Click the link and make an account to see some hop options and even make a favorites lists.
             </MDBCardText>
           </MDBCardBody>
@@ -101,6 +109,9 @@ function Portfolio() {
           <MDBCardBody className="CardBody">
             <MDBCardTitle className='pt-4'>The Bloggers Den</MDBCardTitle>
             <MDBCardText>
+            <a href="https://github.com/AFICRECY/The-Bloggers-Den" target="_blank"
+            rel="noreferrer">(Link to repository)</a>  
+            <br/>
               The Bloggers Den  is a dynamic user-friendly blogging App which allows users to create an account in which
               they can read, update, and delete their own blog posts, as well as
               comment on friends posts.
@@ -123,7 +134,8 @@ function Portfolio() {
           <MDBCardBody className="CardBody">
             <MDBCardTitle className='pt-4'>Daily Timeline Work Planner</MDBCardTitle>
             <MDBCardText>
-            <a href="https://github.com/AFICRECY/daily-timeline-pro">(Link to repository)</a>  
+            <a href="https://github.com/AFICRECY/daily-timeline-pro" target="_blank"
+            rel="noreferrer">(Link to repository)</a>  
             <br/>
             With Daily Timeline Pro, a user can save events for each hour of the day as well as insert them into easily distinguishable time blocks. Time management can be made easy, the user simply clicks into the time block they would like to add an event/task to, and click save, edit, or even delete.
             </MDBCardText>
@@ -145,6 +157,9 @@ function Portfolio() {
           <MDBCardBody className="CardBody">
             <MDBCardTitle className='pt-4'>Weather Pro 5 Day Forecast</MDBCardTitle>
             <MDBCardText>
+            <a href="https://github.com/AFICRECY/weather-pro" target="_blank"
+            rel="noreferrer">(Link to repository)</a>  
+            <br/>
             Weather Pro is a user-friendly weather app that displays daily weather information --temperature, wind, humidity-- and a 5-day forecast for any city entered in the search input. Try out this appealing and easy-to-use weather app!
             </MDBCardText>
           </MDBCardBody>
